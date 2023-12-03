cae085691160723ffff85fa96b537a4564c24b46
Qi-Yangsjtu <53613122+Qi-Yangsjtu@users.noreply.github.com>
2023-12-03 09:29:38 +0800

    Delete Matlab directory

--- Matlab/utils/normalized.m
+++
@@ -1,6 +0,0 @@
-function normalized_data = normalized(input,max)
-originalData = input;
-originalData = int32(originalData);
-originalData(originalData==0)=1;
-originalData(originalData>max)=max;
-normalized_data = originalData;
--- Matlab/utils/similarity.m
+++
@@ -1,3 +0,0 @@
-function sim = similarity(a,b)
-t=eps;
-sim = abs(2.*a.*b+t)./(a.^2+b.^2+t);
